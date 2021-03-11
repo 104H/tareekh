20dabeb9c354d3c515eba8a16084d8b3b5797bea
Hunaid <hunaidhameed@hotmail.com>
2021-03-11 18:45:55 +0100

    added new log functionality

--- tareekh.py
+++ tareekh.py
@@ -2,16 +2,37 @@
 
 import sys
 import os
+import json
 
 def main():
     # initialize new project
-    try:
-        if sys.argv[1] == "new":
-            path = "projects/" + sys.argv[2] + "/"
-            os.mkdir(path, 7777)
-            open(path + "worklog", 'x')
-    except FileExistsError:
-        print("A project by this name already exists. Please pick a unique name")
+    if sys.argv[1] == "new":
+        path = "projects/" + sys.argv[2] + "/"
+
+        if os.path.exists(path):
+            print("A project by this name already exists. Please pick a unique name")
+            return
+
+        os.mkdir(path)
+        open(path + "worklog", 'x')
+
+    if sys.argv[1] == "record":
+        project = sys.argv[2]
+        start = sys.argv[3]
+        end = sys.argv[4]
+        date = sys.argv[5]
+        description = sys.argv[6]
+
+        with open("projects/" + project + "/worklog", "a") as worklog:
+            #log = json.load(worklog)
+            log = {
+                    "date" : date,
+                    "starttime" : start,
+                    "endtime" : end,
+                    "description" : description
+                }
+
+            json.dump(log, worklog)
 
 if __name__ == "__main__":
     main()
